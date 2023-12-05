a65fbeca77e1f54ab53eacafde96bf62dbdc5638
AndreNguyen03 <123562712+AndreNguyen03@users.noreply.github.com>
2023-12-05 19:47:59 +0700

    commit 2 23/5

--- _DoAn/Program.cs
+++ _DoAn/Program.cs
@@ -17,7 +17,7 @@ namespace _DoAn
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Menu());
+            Application.Run(new LoginView());
         }
     }
 }
