fd0eac90889b0645669841d9717ad60ef0cb92fa
AndreNguyen03 <123562712+AndreNguyen03@users.noreply.github.com>
2023-12-22 16:52:08 +0700

    finish forgot password and change password

--- _DoAn/Presenters/LoginPresenter.cs
+++ _DoAn/Presenters/LoginPresenter.cs
@@ -3,18 +3,24 @@ using _DoAn.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
 
 namespace _DoAn.Presenters
 {
     public  class LoginPresenter
     {
         ILoginView loginView;
-
+        string storedOTP;
         public LoginPresenter(ILoginView loginView)
         {
             this.loginView = loginView;
+            storedOTP = generateOTP();
         }
 
         public bool Login()
@@ -47,6 +53,75 @@ namespace _DoAn.Presenters
         {
             User user = new User();
             return user.GetPosition(loginView.username, loginView.password);
+        }
+        public string GetEmail()
+        {
+            User user = new User();
+            return user.GetEmail(loginView.usernameFP);
+        }
+        
+
+        public void SendEmailOTP(string email)
+        {
+            try
+            { 
+
+                var fromAddress = new MailAddress("kiseryouta2003@gmail.com");
+                var toAddress = new MailAddress(email);
+
+                const string fromPass = "ixrb qaxf csve vpvt";
+                const string subject = "OTP code";
+                string body = $"Your OTP code is {storedOTP}";
+
+                var smtp = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(fromAddress.Address, fromPass),
+                    Timeout = 200000
+                };
+                using (var message = new MailMessage(fromAddress, toAddress)
+                {
+                    Subject = subject,
+                    Body = body
+                })
+                {
+                    smtp.Send(message);
+                }
+                MessageBox.Show("OTP is now send to mail!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        
+
+        public string generateOTP()
+        {
+            Random random = new Random();
+            int otp = random.Next(100000, 999999);
+            return otp.ToString();
+        }
+
+        public bool VerifyOTP()
+        {
+            return storedOTP.Equals(loginView.OTP);
+        }
+
+        public bool VerifyNewPassword()
+        {
+            return loginView.newPassword.Equals(loginView.newPasswordAgain);
+        }
+
+        public bool UpdatePassword()
+        {
+            User user = new User();
+            return user.UpdatePassword(loginView.usernameFP,loginView.newPasswordAgain);
         }
     }
 }
