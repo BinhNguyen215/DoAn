a0c047a3e3af1cd3ec7c9ee0da978359b7feb18e
AndreNguyen03 <123562712+AndreNguyen03@users.noreply.github.com>
2023-12-23 15:46:08 +0700

    fix create bill

--- _DoAn/Presenters/SalePresenter.cs
+++ _DoAn/Presenters/SalePresenter.cs
@@ -214,7 +214,7 @@ namespace _DoAn.Presenters
             int startY = 10;
             int offset = 40;
 
-            graphic.DrawString("Green Beauty", new Font("Courier New", 18, FontStyle.Bold), new SolidBrush(Color.Black), startX, startY);
+            graphic.DrawString("Green Pharmacy", new Font("Courier New", 18, FontStyle.Bold), new SolidBrush(Color.Black), startX, startY);
 
             graphic.DrawString("Addresss: 136, Linh Trung, Thủ Đức, TP Thủ Đức", font, new SolidBrush(Color.Black), startX, 40);
 
